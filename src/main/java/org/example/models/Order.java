4d70a91dc3c3196e39c217a9e3c8dbfeef60ca87
ArmineAr <armin.arakelyan@gmail.com>
2025-06-04 23:30:45 +0400

    test

--- src/main/java/org/example/models/Order.java
+++ src/main/java/org/example/models/Order.java
@@ -1,20 +1,20 @@
 package org.example.models;
 
-public class Order {
+import org.example.Exceptions.PaymentException;
+
+public class Order implements Comparable<Order>{
     protected Customer customer;
     protected double price;
     public Order(Customer customer) {
         this.customer = customer;
         this.price = 0;
+        //TODO ?? how to have outgenerated next ID for each order
     }
     public Order(Customer customer, double price) {
         this.customer = customer;
         this.price = price;
     }
 
-    public void printSummery() {
-        System.out.println("Order Price: AMD" + price);
-    }
     public double getPrice() {
         return price;
     }
@@ -27,5 +27,25 @@ public class Order {
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
-
+//    public void shipment(){
+//        if (price <= 0) {
+//            throw new PaymentException("Please make a payment for " + getId());
+//        }
+//        System.out.println("Shipment started for " + getId());
+//    } //TODO dont get how to add IDs
+    @Override
+    public String toString(){
+        return customer+" " + price;  //// ??? dont get how does it go het the toString of cosutmer ?
+    }
+    @Override
+    public int compareTo(Order o) {
+        if(this.price<o.price){
+            return -1;
+        } else if (this.price>o.price) {
+            return 1;
+        }else return 0;
+    }
+    public void printSummary() {
+        System.out.println("Order Price: AMD" + price);
+    }
 }
